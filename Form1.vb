ea860e59273975edc854bd95a73451dbc976403e
MUSIMBI FRANCIS <128372690+Hellysisco@users.noreply.github.com>
2024-09-30 09:13:06 +0300

    Add files via upload
    Variables and Data Types

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        'Get user input from textboxes
+        Dim userName As String = txtUserName.Text
+        Dim userAge As String = txtUserAge.Text
+        Dim favoriteColor As String = txtFavoriteColor.Text
+
+        'Display the message in a label
+        lblResult.Text = "Hello " & userName & ", you are " & userAge & " years old and your favorite color is " & favoriteColor & "."
+    End Sub
+End Class
